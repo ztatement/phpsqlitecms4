87e495d77e8b2fea0ef5dae36f790d370d8fed35
Thomas <ztatements@gmail.com>
2025-01-08 00:22:16 +0100

    update: Fehlerseite ausgelagert

--- cms/includes/exception.inc.php
+++ cms/includes/exception.inc.php
@@ -1,65 +1,75 @@
 <?php
-if(!defined('IN_INDEX')) exit;
+/**
+ * phpSQLiteCMS - ein einfaches und leichtes PHP Content-Management System auf Basis von PHP und SQLite
+ *
+ * @author Mark Hoschek < mail at mark-hoschek dot de >
+ * @copyright (c) 2014 Mark Hoschek
+ * @version last 3.2015.04.02.18.42
+ * @link http://phpsqlitecms.net/
+ * @package phpSQLiteCMS
+ *         
+ * @author Thomas Boettcher <github[at]ztatement[dot]com>
+ * @copyleft () 2025 ztatement
+ * @version 4.5.0.2025.01.07 $Id: cms/includes/exception.inc.php 1 2025-01-07 22:33:21Z ztatement $
+ * @link https://www.demo-seite.com/path/to/phpsqlitecms/
+ * @package phpSQLiteCMS v4
+ *         
+ */
+if (!defined('IN_INDEX'))
+{
+  exit();
+}
 
-$debug_mode = true;
+$debug_mode = true; // In der Produktion wird dieser Wert auf >false< gesetzt.
+
+// Legen wir die Log-Datei fest
+$log_file = BASE_PATH . '.log/_errors.log'; // Pfad zur Log-Datei
 
 $website_title = isset($settings['website_title']) ? $settings['website_title'] : 'phpSQLiteCMS';
-$lang = isset($localization) ? Localization::$lang['lang'] : 'en';
+$lang = isset($localization) ? Localization::$lang['lang'] : 'de';
 $charset = isset($localization) && isset(Localization::$lang['charset']) ? Localization::$lang['charset'] : 'utf-8';
 $exception_title = isset($localization) && isset(Localization::$lang['exception_title']) ? Localization::$lang['exception_title'] : 'Error';
-$exception_message = isset($localization) && isset(Localization::$lang['exception_message']) ? Localization::$lang['exception_message'] : 'An error occurred while processing this directive.';
+$exception_message = isset($localization) && isset(Localization::$lang['exception_message']) ? Localization::$lang['exception_message'] : 'Beim Verarbeiten dieser Anweisung ist ein Fehler aufgetreten.';
 
-header($_SERVER['SERVER_PROTOCOL'] . " 503 Service Unavailable");
+# header($_SERVER['SERVER_PROTOCOL'] . " 503 Service Unavailable");
+$protocol = isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.1';
+header($protocol . " 503 Service Unavailable");
 header("Status: 503 Service Unavailable");
-header('Content-Type: text/html; charset='.$charset);
+header('Content-Type: text/html; charset=' . $charset);
 
-?><!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $lang; ?>">
-<head>
-<title><?php echo $website_title; ?> - <?php echo $exception_title; ?></title>
-<meta http-equiv="content-type" content="text/html; charset=<?php echo $charset; ?>" />
-<style type="text/css">
-<!--
-body              { color:#000; background:#fff; margin:0; padding:0; font-family:verdana,arial,sans-serif; font-size:100.1%; }
-h1                { font-size:1.5em; }
-p,ul              { font-size:0.9em; line-height:1.5em; }
-#top              { margin:0; padding:0 20px 0 20px; height:4em; color:#000000; background:#d2ddea; border-bottom: 1px solid #bacbdf; line-height:4em; }
-#top h1           { font-size:2em; margin:0; padding:0; color:#000080; }
-#content          { padding:20px; }
--->
-</style>
-</head>
-<body>
-<div id="top"><h1><?php echo $website_title; ?></h1></div>
-<div id="content">
-<h1><?php echo $exception_title; ?></h1>
-<p><?php echo $exception_message; ?></p>
+// Wenn wir im Debug-Modus sind, loggen wir die Fehlermeldung
+if ($debug_mode && isset($exception))
+{
+  // Logge den Fehler in der Log-Datei
+  $error_message = "Exception on " . date('d.m.Y H:i:s') . "\n";
+  $error_message .= "Message: " . $exception->getMessage() . "\n";
+  $error_message .= "Code: " . $exception->getCode() . "\n";
+  $error_message .= "File: " . $exception->getFile() . "\n";
+  $error_message .= "Line: " . $exception->getLine() . "\n";
+  $error_message .= "Trace: " . $exception->getTraceAsString() . "\n\n";
 
-<?php if($debug_mode && isset($exception)): ?>
-<p>
-<strong>Message:</strong> <?php echo $exception->getMessage(); ?><br />
-<strong>Code:</strong> <?php echo $exception->getCode(); ?><br />
-<strong>File:</strong> <?php echo $exception->getFile(); ?><br />
-<strong>Line:</strong> <?php echo $exception->getLine(); ?><!--<br />
-<strong>Trace:</strong> <?php echo $exception->getTraceAsString(); ?>-->
-</p>
+  // Schreibe die Fehlerdetails in die Log-Datei
+  error_log($error_message, 3, $log_file);
+}
 
-<?php
-/*
-echo '<p>'.$exception->__toString().'</p>';
-echo '<pre>';
-print_r($exception);
-echo '</pre>';
+// lade das Template für die Fehlerseite
+include (BASE_PATH . 'cms/templates/' . 'exception.template.php');
 
-echo '<pre>';
-print_r($GLOBALS);
-echo '</pre>';
-*/
-?>
-
-<?php endif; ?>
-</p>
-
-</div>
-</body>
-</html>
+/**
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *
+ * @LastModified: 2025-01-07 $Date$ $LastChangedDate: 2025-01-07 22:33:21 +0100 $
+ * @editor: $LastChangedBy: ztatement $
+ * -------------
+ * changelog:
+ * @see change.log
+ *
+ * $Date$ : $Revision$ - Description
+ * 2025-01-07 : 4.5.0.2025.01.07 - auslagern der Fehlerseite "exception.template".
+ *                                 Für die Fehlerausgabe $log_file hinzugefügt
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *
+ * Local variables:
+ * tab-width: 2
+ * c-basic-offset: 2
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
