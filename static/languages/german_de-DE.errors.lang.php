4a812926df3b2ea7c5028ce277835c61eb89c66c
Thomas <ztatements@gmail.com>
2025-01-29 18:52:02 +0100

    added: Errors und Fehlerbeschreibungen

---
+++ static/languages/german_de-DE.errors.lang.php
@@ -0,0 +1,80 @@
+<?php
+/**
+  * Errors und Fehlerbeschreibungen
+  * @version 4.5.0.2025.01.29 
+  * @file $Id: static/languages/german_de-DE.error.lang.php 1 2025-01-08 17:37:00Z ztatement $
+  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+  // Meta-Informaton:
+  $lang['lang']                           = 'de';
+  $lang['charset']                        = 'utf-8';
+  $lang['locale']                         = array('de_DE.utf8', 'de-DE', 'de_DE@euro','de','deu');
+  $lang['time_format']                    = 'd.m.Y, H:i:s'; // kurzes Zeitformat
+  $lang['time_format_full']               = 'A, d. B Y, H:i:s'; // langes Zeitformat
+  $lang['dir']                            = 'ltr';
+
+  // errors
+  $lang['error']                          = 'Fehler!';
+  // datenbank
+  $lang['error_database']                 = 'Datenbankverbindungsfehler:';
+  $lang['db_type_not_supp']               = 'Datenbanktyp nicht unterst&#252;tzt:';
+  // templates
+  $lang['error_Head_template_not_found']  = 'Das <head> Template wurde nicht gefunden.';
+  // content
+  $lang['error_page_name_empty']          = 'Es wurde keine Adresse angegeben';
+  $lang['error_page_name_spec_chars']     = 'Der Seitenname enth&#228;lt ung&#252;ltige Zeichen';
+  $lang['error_page_name_alr_exists']     = 'Der Seitenname ist bereits vorhanden';
+  $lang['error_no_title']                 = 'Es wurde kein Titel angegeben';
+  // admin menu
+  $lang['error_headline']                 = 'Fehler!';
+  // menus
+  $lang['error_menu_spec_chars']          = 'Der Men&#252;-Name enth&#228;lt Sonderzeichen';
+  // gcb
+  $lang['gcb_error_no_identifier']        = 'Es wurde kein Bezeichner angegeben';
+  $lang['gcb_error_invalid_identifier']   = 'Ung&#252;ltiger Bezeichner';
+  // notes
+  $lang['error_note_sect_name_invalid']   = 'Der Notiz-Sektionsname ist ung&#252;ltig (z.Bsp. enth&#228;lt Leer- oder Sonderzeichen)';
+  $lang['error_notes_no_title']           = 'Es wurde kein Titel eingegeben';
+  $lang['error_notes_no_text']            = 'Es wurde kein Text eingegeben';
+  $lang['error_notes_time_invalid']       = 'Die eingegebene Zeit ist ung&#252;ltig';
+  // photos
+  $lang['error_gallery_spec_chars']       = 'Der Name der Fotogalerie enth&#228;lt Sonderzeichen';
+  $lang['error_no_gallery']               = 'Es wurde keine Fotogalerie angegeben';
+  $lang['error_no_thumbnail']             = 'Es wurde kein Thumbnail angegeben';
+  $lang['error_no_photo']                 = 'Es wurde kein Foto angegeben';
+  $lang['error_no_photo_title']           = 'Es wurde kein Titel angegeben';
+  // user
+  $lang['error_username_special_chars']   = 'Der Benutzername enth&#228;lt ung&#252;ltige Zeichen';
+  $lang['error_username_alr_exists']      = 'Der Benutzername ist bereits vergeben.';
+  $lang['error_pw_doesnt_comply']         = 'Das eingegebene Passwort stimmt nicht mit dem wiederholten &#252;berein';
+  $lang['error_form_uncomplete']          = 'Sie haben noch nicht alle Formularfelder ausgef&#252;llt';
+  $lang['error_pw_wrong']                 = 'Falsches Passwort!';
+  // filemanager
+  $lang['error_no_file']                  = 'Es wurde keine Datei zum Hochladen angegeben';
+  $lang['error_no_image']                 = 'Es wurde kein Bild zum Hochladen angegeben';
+  // settings
+  $lang['settings_error_page']            = 'Fehlerseite';
+  $lang['error_settings_spec_chars']      = 'Die Variable enth&#228;lt Sonderzeichen';
+  // spam protection
+  $lang['error_own_ip_banned']            = 'Sie haben Ihre eigene IP gebannt!';
+  $lang['error_own_user_agent_banned']    = 'Sie haben Ihren eigenen User-Agent gebannt!';
+  // exception
+  $lang['exception_title']                = 'Error';
+  $lang['exception_message']              = 'Beim Verarbeiten dieser Anweisung ist ein Fehler aufgetreten.';
+
+/**
+  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *
+  * @LastModified: 2025-01-08 $Date$
+  * @date $LastChangedDate: 2025-01-29 18:49:17 +0100 $
+  * @editor: $LastChangedBy: ztatement $
+  * -------------
+  * @see changelog
+  * $Date$ : $Revision$ - Description
+  * 2025-01-08 : 4.5.0.2025.01.08 - neu angelegt
+  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *
+  * Local variables:
+  * tab-width: 2
+  * c-basic-offset: 2
+  * c-hanging-comment-ender-p: nil
+  * End:
+  */
