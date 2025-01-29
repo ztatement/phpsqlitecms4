79a73e8c3583b6fe00428e085a636b739b80d75d
Thomas <ztatements@gmail.com>
2025-01-29 18:20:08 +0100

    คำอธิบายข้อผิดพลาดเป็นภาษาไทย

---
+++ static/languages/thai_th-TH.error.lang.php
@@ -0,0 +1,64 @@
+<?php
+/**
+  * คำอธิบายข้อผิดพลาดเป็นภาษาไทย
+  * @version 4.5.0.2025.01.29 
+  * @file $Id: static/languages/thai_th-TH.error.lang.php 1 2025-01-29 09:46:00Z ztatement $
+  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+ 
+  // ข้อมูลเมต้า:
+  $lang['lang']                           = 'th';
+  $lang['charset']                        = 'utf-8';
+  $lang['locale']                         = array('th_TH.utf8', 'th-TH','th','thai');
+  $lang['time_format']                    = 'd/m/Y, H:i:s'; // รูปแบบเวลาสั้น
+  $lang['time_format_full']               = 'l, j F Y, H:i:s'; // รูปแบบเวลาเต็ม
+  $lang['dir']                            = 'ltr';
+ 
+  // ข้อผิดพลาด
+  $lang['error']                          = 'ข้อผิดพลาด!';
+  // ฐานข้อมูล
+  $lang['error_database']                 = 'ข้อผิดพลาดในการเชื่อมต่อฐานข้อมูล:';
+  $lang['db_type_not_supp']               = 'ไม่รองรับประเภทฐานข้อมูล:';
+  // แม่แบบ
+  $lang['error_Head_template_not_found']  = 'ไม่พบแม่แบบ <head>';
+  // เนื้อหา
+  $lang['error_page_name_empty']          = 'ไม่มีการระบุที่อยู่';
+  $lang['error_page_name_spec_chars']     = 'ชื่อหน้ามีอักขระที่ไม่ถูกต้อง';
+  $lang['error_page_name_alr_exists']     = 'ชื่อหน้ามีอยู่แล้ว';
+  $lang['error_no_title']                 = 'ไม่มีการระบุชื่อเรื่อง';
+  // เมนูผู้ดูแลระบบ
+  $lang['error_headline']                 = 'ข้อผิดพลาด!';
+  // เมนู
+  $lang['error_menu_spec_chars']          = 'ชื่อเมนูมีอักขระพิเศษ';
+  // gcb
+  $lang['gcb_error_no_identifier']        = 'ไม่มีการระบุรหัส';
+  $lang['gcb_error_invalid_identifier']   = 'รหัสไม่ถูกต้อง';
+  // หมายเหตุ
+  $lang['error_note_sect_name_invalid']   = 'ชื่อส่วนบันทึกไม่ถูกต้อง (เช่น มีช่องว่างหรืออักขระพิเศษ)';
+  $lang['error_notes_no_title']           = 'ไม่มีการระบุชื่อเรื่อง';
+  $lang['error_notes_no_text']            = 'ไม่มีการระบุข้อความ';
+  $lang['error_notes_time_invalid']       = 'เวลาที่ระบุไม่ถูกต้อง';
+  // รูปถ่าย
+  $lang['error_gallery_spec_chars']       = 'ชื่อแกลเลอรีมีอักขระพิเศษ';
+  $lang['error_no_gallery']               = 'ไม่มีการระบุแกลเลอรีภาพ';
+  $lang['error_no_thumbnail']             = 'ไม่มีการระบุภาพขนาดย่อ';
+  $lang['error_no_photo']                 = 'ไม่มีการระบุรูปภาพ';
+  $lang['error_no_photo_title']           = 'ไม่มีการระบุชื่อเรื่อง';
+  // ผู้ใช้
+  $lang['error_username_special_chars']   = 'ชื่อผู้ใช้มีอักขระที่ไม่ถูกต้อง';
+  $lang['error_username_alr_exists']      = 'ชื่อผู้ใช้ถูกใช้ไปแล้ว';
+  $lang['error_pw_doesnt_comply']         = 'รหัสผ่านที่ป้อนไม่ตรงกับที่ป้อนซ้ำ';
+  $lang['error_form_uncomplete']          = 'คุณยังกรอกข้อมูลไม่ครบถ้วน';
+  $lang['error_pw_wrong']                 = 'รหัสผ่านไม่ถูกต้อง!';
+  // ตัวจัดการไฟล์
+  $lang['error_no_file']                  = 'ไม่มีการระบุไฟล์สำหรับอัปโหลด';
+  $lang['error_no_image']                 = 'ไม่มีการระบุรูปภาพสำหรับอัปโหลด';
+  // การตั้งค่า
+  $lang['settings_error_page']            = 'หน้าข้อผิดพลาด';
+  $lang['error_settings_spec_chars']      = 'ตัวแปรมีอักขระพิเศษ';
+  // การป้องกันสแปม
+  $lang['error_own_ip_banned']            = 'คุณได้แบน IP ของตัวเอง!';
+  $lang['error_own_user_agent_banned']    = 'คุณได้แบนตัวแทนผู้ใช้ของตัวเอง!';
+  // ข้อยกเว้น
+  $lang['exception_title']                = 'ข้อผิดพลาด';
+  $lang['exception_message']              = 'เกิดข้อผิดพลาดขณะประมวลผลคำสั่งนี้.';
+
