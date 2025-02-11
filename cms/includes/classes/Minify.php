d5421eaf27ea6a2c62c4137831ef73a0f07b0852
Thomas <ztatements@gmail.com>
2025-02-11 23:15:22 +0100

    update: Code bereinigt und neu kommentiert.

--- cms/includes/classes/Minify.php
+++ cms/includes/classes/Minify.php
@@ -5,71 +5,51 @@
  *
  * @author Thomas Boettcher <github[at]ztatement[dot]com>
  * @copyleft 2016 ztatement
- * @version 4.5.0.2025.01.14 $Id: cms/includes/classes/Minify.php 1 2016-07-18 16:41:09Z ztatement $
+ * @version 4.5.0.2025.02.11 $Id: cms/includes/classes/Minify.php 1 2016-07-18 16:41:09Z ztatement $
  * @link https://www.demo-seite.com/path/to/phpsqlitecms/
  * @package phpSQLiteCMS v4
- *         
+ *
  * @license The MIT License (MIT)
  * @see /LICENSE
- * @see https://opensource.org/licenses/MIT Hiermit wird unentgeltlich jeder Person, die eine Kopie der Software und der zugeh�rigen
- *      Dokumentationen (die "Software") erh�lt, die Erlaubnis erteilt, sie uneingeschr�nkt zu nutzen,
- *      inklusive und ohne Ausnahme mit dem Recht, sie zu verwenden, zu kopieren, zu ver�ndern,
- *      zusammenzuf�gen, zu ver�ffentlichen, zu verbreiten, zu unterlizenzieren und/oder zu verkaufen,
- *      und Personen, denen diese Software �berlassen wird, diese Rechte zu verschaffen,
+ * @see https://opensource.org/licenses/MIT Hiermit wird unentgeltlich jeder Person, die eine Kopie der Software und der zugehörigen
+ *      Dokumentationen (die "Software") erhält, die Erlaubnis erteilt, sie uneingeschränkt zu nutzen,
+ *      inklusive und ohne Ausnahme mit dem Recht, sie zu verwenden, zu kopieren, zu verändern,
+ *      zusammenzufügen, zu veröffentlichen, zu verbreiten, zu unterlizenzieren und/oder zu verkaufen,
+ *      und Personen, denen diese Software überlassen wird, diese Rechte zu verschaffen,
  *      unter den folgenden Bedingungen:
  *     
  *      Der obige Urheberrechtsvermerk und dieser Erlaubnisvermerk sind in allen Kopien
  *      oder Teilkopien der Software beizulegen.
- *     
- *      DIE SOFTWARE WIRD OHNE JEDE AUSDR�CKLICHE ODER IMPLIZIERTE GARANTIE BEREITGESTELLT,
- *      EINSCHLIE?LICH DER GARANTIE ZUR BENUTZUNG F�R DEN VORGESEHENEN ODER EINEM BESTIMMTEN
- *      ZWECK SOWIE JEGLICHER RECHTSVERLETZUNG, JEDOCH NICHT DARAUF BESCHR�NKT.
- *      IN KEINEM FALL SIND DIE AUTOREN ODER COPYRIGHTINHABER F�R JEGLICHEN SCHADEN
- *      ODER SONSTIGE ANSPR�CHE HAFTBAR ZU MACHEN, OB INFOLGE DER ERF�LLUNG EINES VERTRAGES,
+ *
+ *      DIE SOFTWARE WIRD OHNE JEDE AUSDRÜCKLICHE ODER IMPLIZIERTE GARANTIE BEREITGESTELLT,
+ *      EINSCHLIE?LICH DER GARANTIE ZUR BENUTZUNG FÜR DEN VORGESEHENEN ODER EINEM BESTIMMTEN
+ *      ZWECK SOWIE JEGLICHER RECHTSVERLETZUNG, JEDOCH NICHT DARAUF BESCHRÄNKT.
+ *      IN KEINEM FALL SIND DIE AUTOREN ODER COPYRIGHTINHABER FÜR JEGLICHEN SCHADEN
+ *      ODER SONSTIGE ANSPRÜCHE HAFTBAR ZU MACHEN, OB INFOLGE DER ERFÜLLUNG EINES VERTRAGES,
  *      EINES DELIKTES ODER ANDERS IM ZUSAMMENHANG MIT DER SOFTWARE
  *      ODER SONSTIGER VERWENDUNG DER SOFTWARE ENTSTANDEN.
  *      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  */
+
 
 /*
  * html css js compression
  */
 class Minify
 {
-
   // Standardwerte für die Minifizierung
-  /*
-   * protected $compress_css = true;
-   * protected $compress_js = true;
-   * protected $info_comment = false;
-   * protected $remove_comments = true;
-   * protected $html;
-   * public function __construct($html)
-   * {
-   * if (!empty ($html) )
-   * {
-   * $this->parseHTML($html);
-   * }
-   * }
-   */
-  // Standard: CSS komprimieren
-  protected bool $compress_css = true;
+  protected bool $compress_css = true;  // Standard: CSS komprimieren
+  protected bool $compress_js = true;  // Standard: JS komprimieren
+  protected bool $info_comment = true;  // Standard: Keine Info-Kommentare hinzufügen (false) // true = Info-Kommentare hinzufügen
+  protected bool $remove_comments = true;  // Standard: Kommentare entfernen
+  protected string $html = '';  // HTML-String, der komprimiert wird
 
-  // Standard: JS komprimieren
-  protected bool $compress_js = true;
 
-  // Standard: Keine Info-Kommentare hinzufügen (false)
-  protected bool $info_comment = true;
-
-  // true = Info-Kommentare hinzufügen
-
-  // Standard: Kommentare entfernen
-  protected bool $remove_comments = true;
-
-  // HTML-String, der komprimiert wird
-  protected string $html = '';
-
-  // Konstruktor, der das HTML entgegennimmt und die Minifizierung aufruft
+/**
+  * Konstruktor, der das HTML entgegennimmt und die Minifizierung aufruft
+  *
+  * @param string $html  Der HTML-String, der komprimiert werden soll.
+  */
   public function __construct( string $html )
   {
     if (!empty($html))
@@ -78,30 +58,41 @@ class Minify
     }
   }
 
-  // Die __toString-Methode sorgt dafür, dass das Objekt als String zurückgegeben wird
-  # public function __toString( )
+
+/**
+  * Gibt das komprimierte HTML als String zurück
+  *
+  * @return string  Der komprimierte HTML-String.
+  */
   public function __toString( ): string
   {
     return $this->html;
   }
 
-  // Fügt einen Kommentar am Ende hinzu, der die Größe der Reduzierung beschreibt
-  # protected function bottomComment( $raw, $compressed )
+
+/**
+  * Fügt einen Kommentar am Ende hinzu, der die Größe der Reduzierung beschreibt
+  *
+  * @param string $raw  Der ursprüngliche HTML-String.
+  * @param string $compressed  Der komprimierte HTML-String.
+  * @return string  Der Kommentar, der die Reduzierung beschreibt.
+  */
   protected function bottomComment( string $raw, string $compressed ): string
   {
-    # $raw = strlen($raw);
     $raw_length = strlen($raw);
-    # $compressed = strlen($compressed);
     $compressed_length = strlen($compressed);
-    # $savings = ($raw - $compressed) / $raw * 100;
     $savings = ($raw_length - $compressed_length) / $raw_length * 100;
     $savings = round($savings, 2);
-    # return '<!-- HTML Minify | https://www.demo-seite.com/ | Gr&#246;&#223;e reduziert um ' . $savings . '% | Von ' . $raw . ' Bytes, auf ' . $compressed . ' Bytes -->';
     return '<!-- HTML Minify | https://www.demo-seite.com/ | Größe reduziert um ' . $savings . '% | Von ' . $raw_length . ' Bytes, auf ' . $compressed_length . ' Bytes -->';
   }
 
-  // Minifiziert HTML: Entfernt unnötige Leerzeichen, Kommentare und optimiert Tags
-  # protected function minifyHTML( $html )
+
+/**
+  * Minifiziert HTML: Entfernt unnötige Leerzeichen, Kommentare und optimiert Tags
+  *
+  * @param string $html  Der HTML-String, der komprimiert werden soll.
+  * @return string  Der komprimierte HTML-String.
+  */
   protected function minifyHTML( string $html ): string
   {
     $pattern = '/<(?<script>script).*?<\/script\s*>|<(?<style>style).*?<\/style\s*>|<!(?<comment>--).*?-->|<(?<tag>[\/\w.:-]*)(?:".*?"|\'.*?\'|[^\'">]+)*>|(?<text>((<[^!\/\w.:-])?[^<]*)+)|/si';
@@ -113,7 +104,6 @@ class Minify
 
     foreach ($matches as $token)
     {
-      # $tag = (isset($token['tag'])) ? strtolower($token['tag']) : null;
       $tag = $token['tag'] ?? null;
       $content = $token[0];
 
@@ -168,7 +158,6 @@ class Minify
         }
       }
       // Wenn das Tag komprimiert werden soll, Leerzeichen entfernen
-      # $strip = isset($strip) ? $strip : true;
       $strip = $strip ?? true;
       if ($strip)
       {
@@ -179,8 +168,12 @@ class Minify
     return $html;
   }
 
-  // Hauptfunktion zum Minifizieren des HTML-Codes
-  # public function parseHTML( $html )
+
+/**
+  * Hauptfunktion zum Minifizieren des HTML-Codes
+  *
+  * @param string $html  Der HTML-String, der komprimiert werden soll.
+  */
   public function parseHTML( string $html ): void
   {
     $this->html = $this->minifyHTML($html);
@@ -190,44 +183,52 @@ class Minify
     }
   }
 
-  // Entfernt unnötige Leerzeichen aus einem String
-  # protected function removeWhiteSpace( $str )
+
+/**
+  * Entfernt unnötige Leerzeichen aus einem String
+  *
+  * @param string $str  Der String, aus dem die Leerzeichen entfernt werden sollen.
+  * @return string  Der String ohne unnötige Leerzeichen.
+  */
   protected function removeWhiteSpace( string $str ): string
   {
     // Ersetzt alle Vorkommen von Tabulatoren, Zeilenumbrüchen und Wagenrückläufen mit einem einzelnen Leerzeichen
-    # $str = str_replace("\t", ' ', $str);
-    # $str = str_replace("\n", '', $str);
-    # $str = str_replace("\r", '', $str);
     $str = str_replace([
         "\t",
         "\n",
         "\r"
     ], ' ', $str);
-    # while (stristr($str, ' '))
-    # {
-    # $str = str_replace(' ', ' ', $str);
-    # }
-    # return $str;
+
     return preg_replace('/\s+/', ' ', $str); // Ersetzt alle aufeinanderfolgenden Leerzeichen durch ein einzelnes
   }
+
 }
 
-// Funktion, die ein Minify-Objekt erstellt und den HTML-String minifiziert
-# function minify_finish( $html )
-function minify_finish( string $html ): Minify
-{
-  return new Minify($html);
-}
 
-// Funktion, die das Output-Buffering startet und minifiziert
-# function minify_start( )
-function minify_start( ): void
-{
-  ob_start('minify_finish');
-}
-ob_start('minify_finish'); //* Ende der HTML-Kompression */
+/**
+  * Funktion, die ein Minify-Objekt erstellt und den HTML-String minifiziert
+  *
+  * @param string $html Der HTML-String, der komprimiert werden soll.
+  * @return Minify Das Minify-Objekt, das den komprimierten HTML-String enthält.
+  */
+  function minify_finish( string $html ): Minify
+  {
+    return new Minify($html);
+  }
 
- /**
+
+/**
+  * Funktion, die das Output-Buffering startet und minifiziert
+  */
+  function minify_start( ): void
+  {
+    ob_start('minify_finish');
+  }
+  // Starte das Output-Buffering und wendet die Minifizierung an
+  ob_start('minify_finish'); //* Ende der HTML-Kompression */
+
+
+/**
   * Änderungen
   * Änderungen: Typed Properties: Alle Eigenschaften
   * ($compress_css, $compress_js, $info_comment, $remove_comments, $html)
@@ -238,13 +239,14 @@ ob_start('minify_finish'); //* Ende der HTML-Kompression */
   * anstatt die veraltete isset() Methode zu verwenden.
   * 
   * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ *
-  * @LastModified: 2025-01-14 $Date$ $LastChangedDate: 2025-01-14 18:01:24 +0100 $
+  * @LastModified: 2025-02-11 $
+  * @date $LastChangedDate: Tue, 11 Feb 2025 22:13:16 +0100 $
   * @editor: $LastChangedBy: ztatement $
   * -------------
-  * changelog:
   * @see change.log
   *
   * $Date$ $Revision$ - Description
+  * 2025-02-11 : 4.5.0.2025.02.11 : ztatement - update: Code bereinigt und neu kommentiert.
   * 2025-01-14 : 4.5.0.2025.01.14 : ztatement - @fix: Typ bool, string; Verwendung von ??
   * 2024-12-04 : 4.0.4.2024.12.04 : ztatement - @fix: Minify-Klasse variablen (bool und string)
   * 2023-11-01 : 4.0.1.2023.12.04 : ztatement - update: Modifiziert für PHP8
